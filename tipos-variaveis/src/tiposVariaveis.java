4f84cb3a3bda60ff318287e874a701cef3463e6f
Thiago <thiagob2mt@gmail.com>
2025-03-27 21:07:59 -0300

    Praticando tipos e variaveis

--- tipos-variaveis/src/tiposVariaveis.java
+++ tipos-variaveis/src/tiposVariaveis.java
@@ -1,5 +1,16 @@
 public class tiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Tipos primitivos
+
+    int idade = 40;
+    String nome = "Thiago";
+    Float altura = (float) 1.70;
+
+    // Para declarar um constante prefisa do termo "final" e o nomne da variavel em upperCase
+    final String nomeInteiro = "Thiago Barcelos Biancareli";
+    
+    
+
+
     }
 }
